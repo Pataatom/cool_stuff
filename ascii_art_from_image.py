04084b28a39b82bf29d48546351bd918a5fcc99e
Pataatom <pataatom@gmail.com>
2023-11-20 22:01:21 +0100

    Wooohoo, after some time I got it working alright, need to figure out, how to resize the image to look better but still, monochrome and needed to reverse ascii_characters_by_surface

--- ascii_art_from_image.py
+++ ascii_art_from_image.py
@@ -4,42 +4,70 @@ ascii_characters_by_surface_65 = '`^"' + r",:;Il!i~+_-?][}{1)(|\/tfjrxnuvczXYUJC
 ascii_characters_by_surface_10 = ".:-=+*#%@"
 if input("65 or 10> ") in ("10", "ten"):
     ascii_characters_by_surface = ascii_characters_by_surface_10
+
+    # stupid reversing string
+    ascii_list = list(ascii_characters_by_surface)
+    ascii_list.reverse()
+    ascii_characters_by_surface = ""
+    for char in ascii_list:
+        ascii_characters_by_surface += char
+    # stupid reversing string
+
 else:
     ascii_characters_by_surface = ascii_characters_by_surface_65
+    # stupid reversing string
+    ascii_list = list(ascii_characters_by_surface)
+    ascii_list.reverse()
+    ascii_characters_by_surface = ""
+    for char in ascii_list:
+        ascii_characters_by_surface += char
+    # stupid reversing string
 
 
 def pixel_to_ascii(pixel):
-    if isinstance(pixel, int):
-        # Handle grayscale images where the pixel is an integer
-        pixel_brightness = pixel
-    else:
-        # Extract color channels from the pixel tuple
-        (R, G, B, A) = pixel
-        pixel_brightness = 0.299 * R + 0.587 * G + 0.114 * B
-    max_brightness = 255 * 3
+    pixel_brightness = pixel
+    max_brightness = 256
     brightness_weight = len(ascii_characters_by_surface) / max_brightness
     index = int(pixel_brightness * brightness_weight)
-    print(pixel_brightness)
-    if index == 0:
-        return " "
-    else:
-        index -= 1
-        return ascii_characters_by_surface[index]
+    return ascii_characters_by_surface[index]
+
+
+#  ____if rgb than use this____
+'''
+if isinstance(pixel, int):
+    # Handle grayscale images where the pixel is an integer
+    pixel_brightness = pixel
+else:
+    # Extract color channels from the pixel tuple
+    (R, G, B, A) = pixel
+    pixel_brightness = 0.299 * R + 0.587 * G + 0.114 * B
+max_brightness = 255 * 3
+brightness_weight = len(ascii_characters_by_surface) / max_brightness
+index = int(pixel_brightness * brightness_weight)
+print(pixel_brightness)
+if index == 0:
+    return " "
+else:
+    index -= 1
+    return ascii_characters_by_surface[index]
+'''
 
 
 def main(pic):
     image = Image.open(pic)
+    image = image.convert("L")
     (width, height) = image.size
-    if width == height:
+    if True: # width == height:
         new_height = int(height/2.5)
         image = image.resize((width, new_height))
+    
     else:
         new_height = height
-        new_width = width
+    
     ascii_art = []
     for y in range(new_height):
         line = ""
-        for x in range(new_width):
+        for x in range(width):
             px = image.getpixel((x, y))
             line += pixel_to_ascii(px)
         ascii_art.append(line)
@@ -54,4 +82,4 @@ def saving_ascii_art(ascii_art):
 
 
 if __name__ == '__main__':
-    main("asd.png")
+    main(r"C:\Users\pataa\OneDrive\Obrázky\MONKEY.png")
