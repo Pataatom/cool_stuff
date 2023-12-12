1356b0afad42a575e166d4af19130e9395d37b86
Pataatom <pataatom@gmail.com>
2023-12-12 18:35:57 +0100

    Learned a little bit obout cv2 and capturing stream with it.

--- learning/ASCII_VID.py
+++ learning/ASCII_VID.py
@@ -1,2 +1,17 @@
 import cv2
 from curses import wrapper
+
+stream = cv2.VideoCapture(0)
+if not stream.isOpened():
+    raise IOError("Can't open camera")
+while  True:
+    ret, frame = stream.read()
+    if not ret:
+        raise IOError("No more camera for today")
+
+    cv2.imshow("Webcam", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+stream.release()
+cv2.destroyAllWindows()
